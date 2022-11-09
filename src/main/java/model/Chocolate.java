8d8ac1e3a0cf2e8e6b0b465bc08db05e75889b95
SamuelSvahn <samuelsvahn@hotmail.com>
2022-11-09 11:16:23 +0100

    Initial commit

---
+++ src/main/java/model/Chocolate.java
@@ -0,0 +1,21 @@
+package model;
+
+public class Snickers extends Product {
+
+
+    private String
+
+
+
+    public Snickers(int id, double price, String productName) {
+        super(id, price, productName);
+    }
+
+    public String examine() {
+        return null;
+    }
+
+    public String use() {
+        return null;
+    }
+}
---
+++ src/main/java/model/Product.java
@@ -0,0 +1,52 @@
+package se.lexicon;
+
+
+public abstract class Product {
+
+    //fields
+    private int id;
+    private double price;
+    private String productName;
+
+    public Product(int id, double price, String productName) {
+        setId(id);
+        setPrice(price);
+        setProductName(productName);
+    }
+
+
+
+    public abstract String examine();
+
+    public abstract String use();
+
+
+    //setters and getters
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+
+}
